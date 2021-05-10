7a99089c74d923319d99a82407864220b875958f
abelgarcia2 <abelgg2000@gmail.com>
2021-05-11 00:40:47 +0200

    Corregidos pequeños detalles

--- src/main/java/com/abelgarcia2/transformaloYo/controllers/PokemonController.java
+++ src/main/java/com/abelgarcia2/transformaloYo/controllers/PokemonController.java
@@ -1,21 +1,13 @@
 package com.abelgarcia2.transformaloYo.controllers;
 
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-
-import com.abelgarcia2.transformaloYo.models.PokemonModel;
 import com.abelgarcia2.transformaloYo.services.PokemonBDService;
 import com.abelgarcia2.transformaloYo.services.PokemonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PokemonController {
